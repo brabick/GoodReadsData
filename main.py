dc1cf00494a987383e731c8fd126c1a48595d6c8
Brandon <brabick@gmail.com>
2024-01-01 16:48:37 -0700

    authors -> list -> dataframe -> csv

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # This is a sample Python script.
 from collections import Counter
-
+from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def read_csv():
     csv = 'Source/goodreads_library_export (1).csv'
@@ -27,8 +26,30 @@ def read_csv():
     print(dict)
     print(ratings_count)
     print(read_count)
-    print(ratings)
-    ax.bar(ratings, ratings_count)
+    # print(ratings)
+    # ax.bar(ratings, ratings_count)
+    # print(df['Author'])
+    # authors = df['Author']
+    date_str = '01-01-2023'
+    compare_date = datetime.strptime(date_str, '%m-%d-%Y').date()
+    print(type(compare_date))
+    print(compare_date)
+    authors = []
+    for index, row in df.iterrows():
+        # print(row['Date Read'])
+        if len(str(row['Date Read'])) > 3:
+            # print(row['Date Read'])
+            read_date = str(row['Date Read'])
+            read_date.replace('/', '-')
+            # print(read_date)
+            date = datetime.strptime(read_date, '%Y/%m/%d').date()
+
+            if date >= compare_date:
+                print(row['Date Read'])
+                authors.append(row['Author'])
+    authors_df = pd.DataFrame({'author':authors})
+    print(authors_df['author'])
+    authors_df.to_csv('Source/out.csv')
 
 
 # Press the green button in the gutter to run the script.
