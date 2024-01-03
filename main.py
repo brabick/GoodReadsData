f22b91f7fcd925ee7c91242cc0150fd67bb75583
Brandon <brabick@gmail.com>
2024-01-02 19:28:50 -0700

    Clean up initial iteration and creation of authors csv

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 def read_csv():
     csv = 'Source/goodreads_library_export (1).csv'
     df = pd.read_csv(csv)
+    sr = pd.Series(df['Author'])
     print(df.head())
     print(df.info())
     print(df.describe())
@@ -23,13 +24,7 @@ def read_csv():
         for value in read:
             dict[key] = value
 
-    print(dict)
-    print(ratings_count)
-    print(read_count)
-    # print(ratings)
-    # ax.bar(ratings, ratings_count)
-    # print(df['Author'])
-    # authors = df['Author']
+
     date_str = '01-01-2023'
     compare_date = datetime.strptime(date_str, '%m-%d-%Y').date()
     print(type(compare_date))
@@ -37,17 +32,25 @@ def read_csv():
     authors = []
     for index, row in df.iterrows():
         # print(row['Date Read'])
+        print(len(str(row['Date Read'])))
         if len(str(row['Date Read'])) > 3:
+            author_read_count = 0
+
             # print(row['Date Read'])
             read_date = str(row['Date Read'])
             read_date.replace('/', '-')
             # print(read_date)
-            date = datetime.strptime(read_date, '%Y/%m/%d').date()
+            date = datetime.strptime(read_date, '%m/%d/%Y').date()
 
             if date >= compare_date:
-                print(row['Date Read'])
-                authors.append(row['Author'])
-    authors_df = pd.DataFrame({'author':authors})
+                print(row['Date Read'], row['Title'])
+                if row['Author'] not in authors:
+                    authors.append(row['Author'])
+                    print('count = ' + str(sr.value_counts()[row['Author']]))
+    authors_df = pd.DataFrame({'author':authors, 'nationality':'', 'read count':''})
+    for index, row in authors_df.iterrows():
+        print(row['author'] + '!!!!!!!!')
+        row['read count'] = sr.value_counts()[row['author']]
     print(authors_df['author'])
     authors_df.to_csv('Source/out.csv')
 
